8588d904b5635c48be5ccc499c07f2d46d7a294f
carlosgaleano <59021022+carlosgaleano@users.noreply.github.com>
2025-04-09 22:04:05 -0400

    test for css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
 import ProductCard from '@/components/ProductCard';
+import ExampleForProduct from '@/components/ExampleForProduct';
 
 export default function Home() {
   return (
+    <>
+    <h1> productos de ecommerce </h1>
     <main className="flex justify-center items-center min-h-screen bg-gray-100">
       <ProductCard title="Zapatos Gise" price={49.99} />
       <ProductCard title="Zapatos carlos" price={199.99} />
+      <ExampleForProduct />
+      
     </main>
+    </>
+    
   );
 }
